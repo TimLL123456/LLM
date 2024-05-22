8ea9c4c354220b629835afc2463eca4c24178a80
lung lam <lamlung001014@gmail.com>
2024-05-23 01:33:08 +0800

    Fix bug to update full data

--- LLM_with_streamlit/pages/Booking.py
+++ LLM_with_streamlit/pages/Booking.py
@@ -12,25 +12,30 @@ def connect_to_gspreadsheet(date):
     conn = st.connection("gsheets", type=GSheetsConnection)
     df = conn.read(spreadsheet=url, worksheet="Booking", usecols=list(range(3)))
 
-    ### Extract the booking info for the date
-    df = df[df["Date"].astype(str) == str(date)]
-
     return conn, df
 
-def book_or_cancel(conn, df, action:str) -> None:
+def book_or_cancel(conn, full_df, df, date, selected_period, action:str) -> None:
     """Book or Cancel service booking
 
     Args:
         conn (_type_): Connection of the google spreadsheets
-        df (_type_): DataFrame extract from the google spreadsheets
+        full_df (_type_): Full DataFrame extract from the google spreadsheets
+        df (_type_): Selected period DataFrame extract from the google spreadsheets
+        date (_type_): date that user selected
+        selected_period (_type_): Period that user selected
         action (str): "available / cancel"
     """
     ### Change period vacancy status in the dataframe to "available / cancel"
+    ### 2024-05-22 | 07:00 - 08:00 | available -> 2024-05-22 | 07:00 - 08:00 | booked
     _index = df.index[df["Period"] == selected_period].values[0]
     df.loc[_index, "Vacancy"] = action
 
+    ### Update the new df to the full df
+    _indexes = full_df.index[full_df["Date"] == str(date)].values
+    full_df.loc[_indexes, "Vacancy"] = df["Vacancy"].values
+
     ### Update DataFrame
-    conn.update(worksheet="Booking", data=df)
+    conn.update(worksheet="Booking", data=full_df)
 
     ### Refresh whole page
     st.cache_data.clear()
@@ -49,7 +54,10 @@ date = st.date_input("Select a date", value=None)
 if date:
 
     ### Connect to google spreadsheet & Extract data
-    conn, df = connect_to_gspreadsheet(date)
+    conn, full_df = connect_to_gspreadsheet(date)
+
+    ### Extract the booking info for the date
+    df = full_df[full_df["Date"].astype(str) == str(date)]
 
     ### Check if the DataFrame is None
     if not df.empty:
@@ -71,17 +79,17 @@ if date:
 
                 if st.button("Book"):
                     ### Change that period vacancy status
-                    book_or_cancel(conn, df, "booked")
+                    book_or_cancel(conn, full_df, df, date, selected_period, "booked")
 
             if vacancy_status == "booked":
                 st.error("This period is booked")
 
                 if st.button("Cancel"):
                     ### Change that period vacancy status
-                    book_or_cancel(conn, df, "available")
+                    book_or_cancel(conn, full_df, df, date, selected_period, "available")
         
         ### Highlight the period for selected time period
-        st.dataframe(df.style.map(lambda x: "background-color: yellow" if x == selected_period else None), width=600, height=600, hide_index=True)
+        st.dataframe(df.style.applymap(lambda x: "background-color: yellow" if x == selected_period else None), width=600, height=600, hide_index=True)
     
     else:
         
