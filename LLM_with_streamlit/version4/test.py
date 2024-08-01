7c79355006ed49402b3b581ae9540dacc244d9c9
lung lam <lamlung001014@gmail.com>
2024-08-02 00:45:36 +0800

    update check available period function

--- LLM_with_streamlit/version4/test.py
+++ LLM_with_streamlit/version4/test.py
@@ -1,11 +1,48 @@
+import toml
 import streamlit as st
-from st_supabase_connection import SupabaseConnection
-from st_supabase_connection import execute_query
+from datetime import date, datetime
+from supabase import create_client, Client
 
-date = "2024-08-01"
+data = toml.load(".streamlit/secrets.toml")
 
-conn = st.connection("supabase", type=SupabaseConnection)
 
-response = execute_query(conn.table("bookings").select("*").eq("booking_date", date),ttl=0)
+supa_url = data["connections"]["supabase"]["SUPABASE_URL"]
+supa_key = data["connections"]["supabase"]["SUPABASE_KEY"]
+supabase: Client = create_client(supa_url, supa_key)
+
+
+### Booking range: today + 1 month
+class system_info:
+    today = date.today()
+    max_day = datetime(today.year, today.month+1, today.day)
+    period = (7, 23)
+
+
+st.title("📅 Booking System GUI")
+
+
+### Display date input box
+select_date = st.date_input("Select a date",
+                        value=system_info.today,
+                        min_value=system_info.today,
+                        max_value=system_info.max_day)
+
+start_options = [f"{i:02}:00" for i in range(7, 24)]
+start_time = st.selectbox("Select start time", start_options, index=None)
+
+
+end_options = [f"{i:02}:00" for i in range(int(start_time.split(":")[0])+1, 24)]
+end_time = st.selectbox("Select end time", end_options, index=None)
+
+
+### Find if any start time < user select end time & end time > user select start time
+### |------|       |------|
+### 8      10      11     13
+### No record in database, available booking period
+response = supabase.table("bookings").select("*")\
+                                     .eq("booking_date", select_date)\
+                                     .lt("booking_starttime", end_time)\
+                                     .gt("booking_endtime", start_time).execute()
+
 
 response.data
